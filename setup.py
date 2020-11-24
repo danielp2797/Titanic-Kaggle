b7f8056a2f0ec8108258bf39804fff434f99b2b2
daniel <56739984+danielp2797@users.noreply.github.com>
2020-11-24 21:34:13 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this projed a model to predict titanic survivors based on descriptive analysis [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Ca preliminar [[C[Cnaly[2~sis[Ds and someartificial features.',
+    author='Daniel Perez Efremova',
+    license='',
+)
