a03042d73b5cdb9723b91f54f8f3dbee6fee6821
daniel <56739984+danielp2797@users.noreply.github.com>
2020-11-29 22:36:01 +0100

    .csv added to gitignore, simple models developed, directory of submissions changed to data/processed

--- src/data/titanic_submission.py
+++ src/data/titanic_submission.py
@@ -12,7 +12,7 @@ def submit_result(test_prepared, model, features):
     submission_result = pd.DataFrame(list(zip(id_col, model.predict(test_prepared[features]))),
                                      columns=['PassengerId', 'Survived'])
 
-    submission_result.to_csv('../src/data/submission_result.csv', index=False)
-    abs_path = os.path.abspath("../src/data/submission_result.csv")
+    submission_result.to_csv('../data/processed/submission_result.csv', index=False)
+    abs_path = os.path.abspath('../data/processed/submission_result.csv')
 
     api.competition_submit(abs_path, 'API Submission', 'titanic')
